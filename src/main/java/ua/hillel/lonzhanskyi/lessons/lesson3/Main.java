d1427653befd28b8d13b462a770d2c977f1a29ec
Lonzhansky <89276411+Lonzhansky@users.noreply.github.com>
2023-11-27 21:07:32 +0200

    from github

--- src/main/java/ua/hillel/lonzhanskyi/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/lonzhanskyi/lessons/lesson3/Main.java
@@ -3,14 +3,6 @@ package ua.hillel.lonzhanskyi.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("Hello Java");
     }
 }
