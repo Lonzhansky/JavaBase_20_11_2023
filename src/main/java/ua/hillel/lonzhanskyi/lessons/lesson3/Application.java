aaa3c59911d15dd841c8f40c7f19ed1fcb61f457
Lonzhanskyi Denys <lonzhansky@gmail.com>
2023-11-27 21:09:05 +0200

    test 2

---
+++ src/main/java/ua/hillel/lonzhanskyi/lessons/lesson3/Application.java
@@ -0,0 +1,7 @@
+package ua.hillel.lonzhanskyi.lessons.lesson3;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("sdfgshdifusdfugdsufgisd");
+    }
+}
