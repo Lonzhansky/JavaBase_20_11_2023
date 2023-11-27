d4fac7ed0f3c104f7f9fb6ce5580bfb93cb6dd3a
Lonzhanskyi Denys <lonzhansky@gmail.com>
2023-11-27 21:00:54 +0200

    test

--- src/main/java/ua/hillel/lonzhanskyi/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/lonzhanskyi/lessons/lesson3/Main.java
@@ -3,5 +3,10 @@ package ua.hillel.lonzhanskyi.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
     }
 }
